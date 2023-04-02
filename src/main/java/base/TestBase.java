df94a1ec31cab0903adb8ae19a45e5fc77262e30
NicolaeBunea <nicolae_bunea23@yahoo.com>
2023-04-03 01:24:15 +0300

    created pak

---
+++ src/main/java/base/TestBase.java
@@ -0,0 +1,28 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Properties;
+
+public class TestBase {
+
+    private WebDriver driver;
+    private Properties prop;
+    @Before
+    public void initiateDriver(){
+        String browserName = prop.getProperty("browser");
+        if (browserName.equals("chrome")){
+            System.setProperty("webdriver.chrome.driver","C:\\Automation\\chromedriver.exe");
+            driver= new ChromeDriver();
+        }
+    }
+
+
+
+
+
+
+
+
+}
